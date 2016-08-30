fb17fc939ab7f17d1a9d78d53dd68036ea9fec3b
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-08-30 18:27:19 +0200

    feat(tabs): add indicators to RSVP filter tabs

--- views/default/filters/events/rsvps.php
+++ views/default/filters/events/rsvps.php
@@ -12,9 +12,16 @@ $rsvp_options[] = 'invited';
 
 $tabs = array();
 foreach ($rsvp_options as $option) {
+	$counter = elgg_get_entities_from_relationship([
+		'relationship' => $options,
+		'relationship_guid' => $entity->guid,
+		'inverse_relationship' => true,
+		'count' => true,
+	]);
+	
 	$tabs[] = array(
 		'name' => $option,
-		'text' => elgg_echo("events:rsvp:list:$option"),
+		'text' => elgg_echo("events:rsvp:list:$option") . '<span class="indicator">' . $counter . '</span>',
 		'href' => "/calendar/events/rsvps/$entity->guid/$option",
 		'selected' => $option == $filter_context,
 	);
@@ -27,5 +34,6 @@ foreach ($tabs as $tab) {
 echo elgg_view_menu('filter', array(
 	'entity' => $entity,
 	'sort_by' => 'priority',
+	'class' => 'events-rsvp-filter',
 ));
 
